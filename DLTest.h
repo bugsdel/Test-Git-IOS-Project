95026bf488acb9eed2e261f15c905bc0a1c18ab6
Daniel Lozano <ing.lozano@gmail.com>
2014-09-09 18:19:08 -0500

    added Test class

---
+++ DLTest.h
@@ -0,0 +1,13 @@
+//
+//  DLTest.h
+//  Git Test
+//
+//  Created by Daniel Lozano Náñez on 9/9/14.
+//  Copyright (c) 2014 esau.mx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DLTest : NSObject
+
+@end
---
+++ DLTest.m
@@ -0,0 +1,13 @@
+//
+//  DLTest.m
+//  Git Test
+//
+//  Created by Daniel Lozano Náñez on 9/9/14.
+//  Copyright (c) 2014 esau.mx. All rights reserved.
+//
+
+#import "DLTest.h"
+
+@implementation DLTest
+
+@end
